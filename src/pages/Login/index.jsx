5ee1222d49529edcf2cc8f40a085ff5444936a8d
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-07-04 16:19:13 -0300

    mobile layout added to login

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
-import { Container, LoginContainer } from './styles';
+import { 
+  Container, 
+  LoginContainer, 
+  Logo, 
+  Description, 
+  Form, 
+  InputContainer, 
+  Input, 
+  SignInBtn,
+  Person,
+  Lock, 
+} from './styles';
+import logonormal from '../../assets/logo-normal.png';
 
 function Login() {
   return (
     <Container>
-      <LoginContainer />
+      <LoginContainer>
+        <Logo src={logonormal}/>
+        <Description>O MAIOR CLUBE DE CERVEJEIROS DO BRASIL</Description>
+
+        <Form>
+          <InputContainer>
+            <Person />
+            <Input placeholder="Identificador" />
+          </InputContainer>
+
+          <InputContainer>
+            <Lock />
+            <Input placeholder="Senha"/>
+          </InputContainer>
+
+          <SignInBtn type="button">Iniciar Sessão</SignInBtn>
+
+        </Form>
+
+        <Description color="#555">
+          Faça login para ter acesso ao painel administrativo de seu estabelecimento.
+        </Description>
+      </LoginContainer>
     </Container>
   );
 }
--- src/pages/Login/styles.jsx
+++ src/pages/Login/styles.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
+import { BsFillPersonFill, BsLock } from 'react-icons/bs';
 
 import login from '../../assets/login.jpg';
+import { MEDIA } from '../../App';
 
 export const Container = styled.div`
   background-image: url(${login});
@@ -14,9 +16,77 @@ export const Container = styled.div`
   justify-content: center;
 `;
 
+export const Form = styled.form`
+  margin: 40px 0;
+`;
+
 export const LoginContainer = styled.div`
   background-color: white;
   width: 400px;
   height: 600px;
   border-radius: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px 20px;
+
+  ${MEDIA} {
+    width: 100%;
+    height: 100%;
+    border-radius: 0;
+  }
 `;
+
+export const Logo = styled.img`
+  width: 70%;
+  margin-bottom: 5px;
+`;
+
+export const Description = styled.p`
+  text-align: center;
+  font-size: 25px;
+  ${({ color }) => color ? `color: ${color}` : ''}
+
+  ${MEDIA} {
+    font-size: 20px;
+  }
+`;
+
+export const InputContainer = styled.div`
+  margin: 10px 0;
+  border: solid 3px #FFCC29;
+  height: 50px;
+  border-radius: 25px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding-left: 10px;
+`;
+
+export const Input = styled.input`
+  background: none;
+  height: 100%;
+  border: none;
+  padding: 0 10px;
+  color: black;
+  font-size: 18px;
+`;
+
+export const SignInBtn = styled.button`
+  background-color: #FFCC29;
+  height: 50px;
+  border-radius: 25px;
+  width: 100%;
+  border: none;
+  margin-top: 20px;
+  font-size: 22px;
+
+  &:hover {
+    cursor: pointer;
+    filter: brightness(70%);
+  }
+`;
+
+export const Person = styled(BsFillPersonFill)``;
+
+export const Lock = styled(BsLock)``;
