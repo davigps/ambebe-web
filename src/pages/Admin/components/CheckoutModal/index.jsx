88c98561273273e37c1871457c42d911a14de5d0
Davi Sousa <davi.gomes.sousa@ccc.ufcg.edu.br>
2020-07-05 11:13:24 -0300

    base checkout modal added

--- src/pages/Admin/components/CheckoutModal/index.jsx
+++ src/pages/Admin/components/CheckoutModal/index.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import { Modal } from 'react-responsive-modal';
 
-// import { Container } from './styles';
+import {
+  Container, Header, PersonContainer, ClockContainer, Person, Text, Clock,
+  List, Product, Quantity, Confirm, ConfirmButton,
+} from './styles';
 
-function CheckoutModal() {
-  return <div />;
+function CheckoutModal({
+  name, time, orders, onConfirm,
+}) {
+  return (
+    <Modal>
+      <Container>
+        <Header>
+          <PersonContainer>
+            <Person />
+            <Text>{name}</Text>
+          </PersonContainer>
+
+          <ClockContainer>
+            <Clock />
+            <Text>{time}</Text>
+          </ClockContainer>
+        </Header>
+
+        <List>
+          {
+            orders.map((item) => (
+              <Product>
+                <Text>{item.name}</Text>
+                <Quantity>
+                  x
+                  {item.quantity}
+                </Quantity>
+              </Product>
+            ))
+          }
+        </List>
+
+        <Confirm>
+          <ConfirmButton onClick={onConfirm}>
+            Confirmar
+          </ConfirmButton>
+        </Confirm>
+      </Container>
+    </Modal>
+  );
 }
 
 export default CheckoutModal;
--- src/pages/Admin/components/CheckoutModal/styles.js
+++ src/pages/Admin/components/CheckoutModal/styles.js
@@ -1,5 +1,25 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  
-`;
+export const Container = styled.div``;
+
+export const Header = styled.div``;
+
+export const PersonContainer = styled.div``;
+
+export const ClockContainer = styled.div``;
+
+export const Person = styled.div``;
+
+export const Text = styled.div``;
+
+export const Clock = styled.div``;
+
+export const List = styled.div``;
+
+export const Product = styled.div``;
+
+export const Quantity = styled.div``;
+
+export const Confirm = styled.div``;
+
+export const ConfirmButton = styled.div``;
